93a12a94df3dffd1bf22fde197fbced1579f72c3
Hryhorii Opanasenko <hryhoriiopanasenko@MacBook-Pro-Hryhorii.local>
2021-04-15 09:24:01 +0300

    preview image

--- test-front/src/app/app.component.ts
+++ test-front/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test-front';
+  url: any;
+  onSelectFile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event) => {
+        this.url = event.target.result;
+      };
+    }
+  }
 }
